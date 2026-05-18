84292daa27040fe390aa8ffbfadefdd5e438de77
alxfed <alxfed@gmail.com>
2026-05-18 07:05:42 -0500

    corrections

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -14,14 +14,14 @@ def test_uvx_integration():
     
     input_text = "Theodotos-Alexandreus: Are language models seeking the Truth, machine?\n"
     
-    # "uvx --from . name-of-the-machine" behaves identically to PyPI "uvx name-of-the-machine"
+    # "uvx --from . reading-machine" behaves identically to PyPI "uvx reading-machine"
     cmd = [
         "uvx", 
         "--with",
         ".[all]",
         "--from", 
         ".", 
-        "name-of-the-machine"
+        "reading-machine"
     ]
     
     # The keys/tokens are passed through the environment automatically via env=env.
